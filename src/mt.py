61a651bd4b2921640e1218f71744149db6dba4b4
Christian <chris3151@ciencias.unam.mx>
2019-06-05 13:49:09 -0500

    Chaos

--- src/mt.py
+++ src/mt.py
@@ -21,13 +21,51 @@ class TM(object):
             
         except Exception as c:
             print(c.args[0])
-            
 
+
+    def write_string(self):
+        self.cinta = []
+        
+        for sym in self.string:
+            self.cinta.append(sym)
+            
+        self.cinta.append('_')
+        self.cinta.append('_')
+        self.trans = self.tm['Transiciones']
+        self.q = self.tm['Inicial']
+        self.first_sym = self.cinta[0]
+        self.l = []
+        self.l1 = []
+        self.p_string = ""
+        self.l1.append(self.q)
+        
+        for sym in self.cinta:
+            self.l1.append(sym)
+            
+        self.l.append(self.l1)
+        
+
+    def read_t(self):
+        self.l1 = []
+        
+        for t in self.trans:
+            
+            if(self.q == t[0] and self.first_sym == t[1]):
+                self.q = t[2]
+                self.p_string.append(self.first_sym)
+                self.first_sym = t[3]
+                
+                if(t[4] == 'R'):
+                    for sym in self.p_string:
+                        self.l1.append(sym)
+                    
 
 if __name__ == '__main__':
     args = sys.argv
     if(len(args) > 3):
         print('Sólo se necesitan 2 argumentos; el archivo .json con la descripción de la máquina de Turing y la cadena a procesar')
     else:
-        TM = TM(args[1],args[0])
+        TM = TM(args[1],args[2])
+        TM.write_string()
+        TM.read_t()
         
